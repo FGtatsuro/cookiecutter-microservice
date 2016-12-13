96e0ca236e0ab3efb16d9c13d134c9db72a8dfea
FGtatsuro <fujiistorage@gmail.com>
2016-12-13 19:33:54 +0900

    Ruby uses 2 spaces as indent.

--- {{cookiecutter.project_name}}/spec/{{cookiecutter.project_name}}_spec.rb
+++ {{cookiecutter.project_name}}/spec/{{cookiecutter.project_name}}_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper_#{ENV['TARGET_BACKEND']}"
 
 describe file('/opt/{{ cookiecutter.project_name }}/services.sh') do
-    it { should be_file }
-    it { should be_mode 755 }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
